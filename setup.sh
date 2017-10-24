1488dd08764a4bfd3c46624f51c0005cd1d4e0c3
Romain Giraud <giraud.romain@gmail.com>
2017-10-24 16:31:23 +0200

    setup: add an install script
    relocate dotfiles (cleaning)

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+declare -a BIN_FILES=(
+    'bin/bd'
+    'bin/mpager'
+    'bin/plompt'
+    'bin/renamer'
+    'bin/repo-each'
+)
+
+declare -A HOME_FILES=(
+    ['.bashrc']='home/bashrc'
+    ['.gdbinit']='home/gdbinit'
+    ['.gitconfig']='home/gitconfig'
+    ['.tmux.conf']='home/tmux.conf'
+    ['.vimrc']='home/vimrc'
+    ['.Xresources']='home/Xresources'
+)
+
+for f in ${BIN_FILES[@]}; do
+    ln -s "$(realpath $f)" "$HOME/bin/$(basename $f)"
+done
+
+for f in ${!HOME_FILES[@]}; do
+    ln -s "$(realpath ${HOME_FILES[$f]})" "$HOME/$(basename $f)"
+done
